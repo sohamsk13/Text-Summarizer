5028140dc8ed9b4e99d55562dbca87838b6aabf9
nishant16101 <nishantkumbhar812@gmail.com>
2025-01-11 23:56:11 +0530

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f"stage {STAGE_NAME}initiated")
@@ -21,3 +24,26 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME="Model Trainer stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
